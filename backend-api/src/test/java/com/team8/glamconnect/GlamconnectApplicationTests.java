fd46408cb009ae955ab578a13f3bafbe046d61df
Ndeye-N <nmndiaye@uncg.edu>
2025-10-28 00:42:42 -0400

    Milestone 5: add Spring Boot scaffold in backend-api (Web, JPA, Postgres)

---
+++ backend-api/src/test/java/com/team8/glamconnect/GlamconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team8.glamconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
